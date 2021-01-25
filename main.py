40ef15731506109f2fcfc67c0a375a1e8e1e0df2
sm0862512 <Sm0862512@otc.edu>
2021-01-25 18:24:11 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 songs = 0
 dollarprice = 0.0
-
-songs = int (input (" enter number of songs you wish to buy today: "))
-dollarprice = 0.99 * songs
-print (dollarprice)
+songs = eval (input (" Enter the number of songs you wish to purcasetoday: "))
+dollarprice = 0.99 + songs
+print ("the price of your purcase is", dollarprice)
